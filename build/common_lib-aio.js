51d4b82abe34a3eb41a345d2bd3c8fb3e6d0280b
hudamin <hudamin@baidu.com>
2013-08-30 10:42:55 +0800

    add js subdirectory

--- build/common_lib-aio.js
+++
@@ -1,3 +0,0 @@
-__inline("../libs/zepto-1.0rc1/zepto.js");
-__inline("../libs/underscore-1.4.4/underscore.js");
-__inline("../libs/backbone-0.9.10/backbone.js");
---
+++ build/js/common_rocket-aio.js
@@ -0,0 +1,14 @@
+__inline("../../src/rocket.js");
+__inline("../../src/rocket.baseview.js");
+__inline("../../src/rocket.collection.js");
+__inline("../../src/rocket.model.js");
+__inline("../../src/rocket.pageview.js");
+__inline("../../src/rocket.subpageview.js");
+__inline("../../src/rocket.router.js");
+__inline("../../src/rocket.subview.js");
+__inline("../../src/rocket.globalview.js");
+__inline("../../src/rocket.subpagemanager.js");
+__inline("../../src/pageanimations/rocket.pageanimation_dropdown.js");
+__inline("../../src/pageanimations/rocket.pageanimation_fade.js");
+__inline("../../src/pageanimations/rocket.pageanimation_simple.js");
+__inline("../../src/pageanimations/rocket.pageanimation_slide.js");
