a0b204d3ae94745378d49ac02484729ac4b3ead5
hudamin <hudamin@baidu.com>
2013-10-01 22:50:55 +0800

    add backbone-1.0.0

--- build/js/common_lib-aio.js
+++ build/js/common_lib-aio.js
@@ -1,3 +1,3 @@
 __inline("../../libs/zepto-1.0rc1/zepto.js");
 __inline("../../libs/underscore-1.4.4/underscore.js");
-__inline("../../libs/backbone-0.9.10/backbone.js");
+__inline("../../libs/backbone-1.0.0/backbone.js");
